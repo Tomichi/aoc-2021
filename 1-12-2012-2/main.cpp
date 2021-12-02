7d05f34b6d167884f5214c00d59e46bd3328a186
Tomáš Michna <Michna12@gmail.com>
2021-12-02 01:01:27 +0100

    reduce size of lines

--- 1-12-2012-2/main.cpp
+++ 1-12-2012-2/main.cpp
@@ -3,17 +3,14 @@
 #include <array>
 
 int main() {
-    bool is_reading_ok = false;
+    bool is_array_fill = false;
     std::array<int, 4> numbers;
     int index = 0;
     int increased_num = 0;
     while(std::cin >> numbers[index]) {
-	if (!is_reading_ok && index == 3) {
-	    is_reading_ok = true;
-        }
-     increased_num += is_reading_ok && numbers[(index + 5) % 4] < numbers[index] ? 1 : 0;
-     index = (index + 1) % 4;
-            
+       is_array_fill = is_array_fill || (!is_array_fill && index == 3);
+       increased_num += is_array_fill && numbers[(index + 1) % 4] < numbers[index] ? 1 : 0;
+       index = (index + 1) % 4;
     }
     std::cout << increased_num << "\n";
 return 0;
